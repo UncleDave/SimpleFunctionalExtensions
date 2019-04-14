a802a21cc476ef6edbb1ece4d51a75e76a9ec922
Dave Pringle <3244206+UncleDave@users.noreply.github.com>
2019-04-14 16:28:49 +0100

    Added XML Docs for CommandResult

--- SimpleFunctionalExtensions/CommandResult.cs
+++ SimpleFunctionalExtensions/CommandResult.cs
@@ -1,24 +1,41 @@
 ﻿namespace SimpleFunctionalExtensions
 {
+    /// <inheritdoc cref="ICommandResult"/>
     public class CommandResult : Result
     {
         private CommandResult(bool isSuccess) : base(isSuccess) { }
 
+        /// <summary>
+        /// Creates a successful <see cref="ICommandResult"/>.
+        /// </summary>
+        /// <returns>A new <see cref="ICommandResult"/> in the success state.</returns>
         public static ICommandResult Ok() => new CommandResult(true);
 
+        /// <summary>
+        /// Creates a failed <see cref="ICommandResult"/>.
+        /// </summary>
+        /// <returns>A new <see cref="ICommandResult"/> in the failure state.</returns>
         public static ICommandResult Fail() => new CommandResult(false);
     }
 
+    /// <inheritdoc cref="ICommandResult{T}"/>
     public class CommandResult<T> : Result, ICommandResult<T>
     {
+        /// <inheritdoc cref="ICommandResult{T}.Error"/>
         public T Error { get; }
 
         private CommandResult() : base(true) { }
 
         private CommandResult(T error) : base(false) => Error = error;
 
+        /// <inheritdoc cref="CommandResult.Ok"/>
         public static ICommandResult<T> Ok() => new CommandResult<T>();
 
+        /// <summary>
+        /// Creates a failed <see cref="ICommandResult"/> with the given error.
+        /// </summary>
+        /// <param name="error">The error that caused the failure.</param>
+        /// <returns>A new <see cref="ICommandResult"/> in the failure state with the given error.</returns>
         public static ICommandResult<T> Fail(T error) => new CommandResult<T>(error);
     }
 }
--- SimpleFunctionalExtensions/ICommandResult.cs
+++ SimpleFunctionalExtensions/ICommandResult.cs
@@ -1,14 +1,32 @@
 ﻿namespace SimpleFunctionalExtensions
 {
+    /// <summary>
+    /// Represents the successful or unsuccessful outcome of a task.
+    /// </summary>
     public interface ICommandResult
     {
+        /// <summary>
+        /// Gets whether this <see cref="ICommandResult"/> is a success.
+        /// </summary>
         bool IsSuccess { get; }
 
+        /// <summary>
+        /// Gets whether this <see cref="ICommandResult"/> is a failure.
+        /// </summary>
         bool IsFailure { get; }
     }
 
+    /// <inheritdoc cref="ICommandResult"/>
+    /// <summary>
+    /// Unsuccessful results contain an error of type <see cref="T"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of the error produced when the task fails.</typeparam>
     public interface ICommandResult<out T> : ICommandResult
     {
+        /// <summary>
+        /// The error that caused this <see cref="ICommandResult{T}"/> to fail.
+        /// Null if <see cref="ICommandResult.IsSuccess"/> is true.
+        /// </summary>
         T Error { get; }
     }
 }
