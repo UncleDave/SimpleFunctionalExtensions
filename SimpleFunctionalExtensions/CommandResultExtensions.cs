62523d34e071063b2e5c11c60d2e6deb7da6177c
Dave Pringle <3244206+UncleDave@users.noreply.github.com>
2019-06-03 22:48:19 +0100

    Added CatchWhen and Catch overloads with parameterless errorHandler

--- SimpleFunctionalExtensions/CommandResultExtensions.cs
+++ SimpleFunctionalExtensions/CommandResultExtensions.cs
@@ -9,5 +9,24 @@ namespace SimpleFunctionalExtensions
 
         public static CommandResult<T> Catch<T>(this CommandResult<T> result, Func<T, CommandResult<T>> errorHandler) =>
             result.IsFailure ? errorHandler(result.Error) : result;
+
+        public static CommandResult<T> Catch<T>(this CommandResult<T> result, Func<CommandResult<T>> errorHandler) =>
+            result.IsFailure ? errorHandler() : result;
+
+        public static CommandResult<T> CatchWhen<T>(this CommandResult<T> result, Func<T, bool> condition, Func<T, CommandResult<T>> errorHandler)
+        {
+            if (result.IsFailure)
+                return condition(result.Error) ? errorHandler(result.Error) : result;
+
+            return result;
+        }
+
+        public static CommandResult<T> CatchWhen<T>(this CommandResult<T> result, Func<T, bool> condition, Func<CommandResult<T>> errorHandler)
+        {
+            if (result.IsFailure)
+                return condition(result.Error) ? errorHandler() : result;
+
+            return result;
+        }
     }
 }
