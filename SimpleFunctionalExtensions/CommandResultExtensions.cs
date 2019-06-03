5c5f157857ebdca58bc302c8e94d1cc8437c0d23
Dave Pringle <3244206+UncleDave@users.noreply.github.com>
2019-06-03 22:11:55 +0100

    Changed Catch's errorHandler argument to be a Func that returns a concrete type instead of an interface; this facilitates the implicit operator on the concrete QueryResult types

--- SimpleFunctionalExtensions/CommandResultExtensions.cs
+++ SimpleFunctionalExtensions/CommandResultExtensions.cs
@@ -4,10 +4,10 @@ namespace SimpleFunctionalExtensions
 {
     public static class CommandResultExtensions
     {
-        public static ICommandResult Catch(this ICommandResult result, Func<ICommandResult> errorHandler) =>
+        public static ICommandResult Catch(this ICommandResult result, Func<CommandResult> errorHandler) =>
             result.IsFailure ? errorHandler() : result;
 
-        public static ICommandResult<T> Catch<T>(this ICommandResult<T> result, Func<T, ICommandResult<T>> errorHandler) =>
+        public static ICommandResult<T> Catch<T>(this ICommandResult<T> result, Func<T, CommandResult<T>> errorHandler) =>
             result.IsFailure ? errorHandler(result.Error) : result;
     }
 }
--- SimpleFunctionalExtensions/QueryResultExtensions.cs
+++ SimpleFunctionalExtensions/QueryResultExtensions.cs
@@ -4,10 +4,10 @@ namespace SimpleFunctionalExtensions
 {
     public static class QueryResultExtensions
     {
-        public static IQueryResult<T> Catch<T>(this IQueryResult<T> result, Func<IQueryResult<T>> errorHandler) =>
+        public static IQueryResult<T> Catch<T>(this IQueryResult<T> result, Func<QueryResult<T>> errorHandler) =>
             result.IsFailure ? errorHandler() : result;
 
-        public static IQueryResult<TValue, TError> Catch<TValue, TError>(this IQueryResult<TValue, TError> result, Func<TError, IQueryResult<TValue, TError>> errorHandler) =>
+        public static IQueryResult<TValue, TError> Catch<TValue, TError>(this IQueryResult<TValue, TError> result, Func<TError, QueryResult<TValue, TError>> errorHandler) =>
             result.IsFailure ? errorHandler(result.Error) : result;
     }
 }
